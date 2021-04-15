0e1facee1d1140376e30d83f361187d74bb69f6b
kevin <kevinbhavsar9@gmail.com>
2021-04-16 03:24:57 +0530

    set view engine

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const port =8000;
 // all the request are mapped to routes folder
 app.use('/',require('./routes'));
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views')
+
 
 
 app.listen(port,function(err){
